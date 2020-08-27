3031e6f71f0a95f17b834e60a6523d23b5756280
Xiangru Lian <admin@mail.xrlian.com>
2020-08-27 08:26:56 -0700

    [feat] add bench for sharded

---
+++ benches/sharded.rs
@@ -0,0 +1,75 @@
+use bustle::*;
+use sharded::{Map, ShardLock};
+
+#[derive(Clone)]
+struct Table<K>(std::sync::Arc<Map<K, ()>>);
+
+impl<K> Collection for Table<K>
+where
+    K: Send + Sync + From<u64> + Copy + 'static + std::hash::Hash + Eq + std::fmt::Debug,
+{
+    type Handle = Self;
+    fn with_capacity(capacity: usize) -> Self {
+        Self(std::sync::Arc::new(Map::with_capacity(capacity)))
+    }
+
+    fn pin(&self) -> Self::Handle {
+        self.clone()
+    }
+}
+
+impl<K> CollectionHandle for Table<K>
+where
+    K: Send + From<u64> + Copy + 'static + std::hash::Hash + Eq + std::fmt::Debug,
+{
+    type Key = K;
+
+    fn get(&mut self, key: &Self::Key) -> bool {
+        self.0.read(key).get(key).is_some()
+    }
+
+    fn insert(&mut self, key: &Self::Key) -> bool {
+        !self.0.write(key).insert(*key, ()).is_some()
+    }
+
+    fn remove(&mut self, key: &Self::Key) -> bool {
+        self.0.write(key).remove(key).is_some()
+    }
+
+    fn update(&mut self, key: &Self::Key) -> bool {
+        use hashbrown::hash_map::Entry;
+        let mut map = self.0.write(key);
+        if let Entry::Occupied(mut e) = map.entry(*key) {
+            e.insert(());
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn main() {
+    tracing_subscriber::fmt::init();
+    println!("embedding server");
+    for n in (1..=2 * num_cpus::get()).step_by(num_cpus::get() / 4) {
+        Workload::new(
+            n,
+            Mix {
+                read: 90,
+                insert: 10,
+                remove: 0,
+                update: 0,
+                upsert: 0,
+            },
+        )
+        .run::<Table<u64>>();
+    }
+    println!("read heavy");
+    for n in (1..=2 * num_cpus::get()).step_by(num_cpus::get() / 4) {
+        Workload::new(n, Mix::read_heavy()).run::<Table<u64>>();
+    }
+    println!("uniform");
+    for n in (1..=2 * num_cpus::get()).step_by(num_cpus::get() / 4) {
+        Workload::new(n, Mix::uniform()).run::<Table<u64>>();
+    }
+}
